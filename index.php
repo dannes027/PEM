5ba353b3ad35d1013f3f93384eb30c825c3a4af4
dannes027 <44549558+dannes027@users.noreply.github.com>
2023-05-24 13:45:13 -0600

    index.php

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="https://procesosespeciales.com/css/index.css">
+    <link rel="stylesheet" href="https://procesosespeciales.com/css/index_preload.css"> -->
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://procesosespeciales.com/assets/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://procesosespeciales.com/assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://procesosespeciales.com/assets/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="https://procesosespeciales.com/assets/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="https://procesosespeciales.com/assets/android-chrome-512x512.png">
+    <link rel="manifest" href="https://procesosespeciales.com/assets/site.webmanifest">
+
+    <link rel="canonical" href="https://procesosespeciales.com/">
+    <meta name="robots" content="index,follow">
+    <meta name="googlebot" content="index,follow">
+    <meta name="author" content="Ing. Nestor Daniel García Yescas"/>
+    <meta name="copyright" content="Copyright 2023 procesosespeciales.com, Todos los Derechos Reservados">
+    <meta name="description"
+        content="En PEM manejamos los mejores procesos en diseño, pintura electrostática así como en las maquilas de productos metálicos como gabinetes metálicos, racks industriales y todo en la rama metalmecánica." />
+    <meta name="keywords"
+        content="pintrua electrostatica, aplicacion de pintura electrostatica, pintura en polvo, pintura electrostatica en CDMX, metalmecanica, productos metalicos, corte laser" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="PEM | Procesos Especiales y Manufactura" />
+    <meta property="og:description"
+        content="En PEM manejamos los mejores procesos en diseño, pintura electrostática así como en las maquilas de productos metálicos como gabinetes metálicos, racks industriales y todo en la rama metalmecánica." />
+    <meta property="og:url" content="https://procesosespeciales.com/" />
+    <meta property="og:site_name" content="Procesos Especiales y Manufactura PEM" />
+    <title>PEM | Procesos Especiales y Manufactura</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="menu-header">
+                <img src="assets/img/menu.svg" alt="">
+            </div>
+            <div class="enlaces-header" id="enlaces">
+                <a href="#">Inicio</a>
+                <a href="#">Nosotros</a>
+                <a href="#">Servicios</a>
+                <a href="#">Galeria</a>
+                <a href="#">Contacto</a>
+                <a href="#">Otro</a>
+            </div>
+        </nav>
+        <!-- <div class="contenido-header">
+            <div class="textos-header">
+                <h1>Creamos el sitio ideal para ti</h1>
+                <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores, quos!</h2>
+                <a href="#">Ver más.</a>
+            </div>
+            <div class="img-header">
+                <img src="assets/undraw_design_notes_8dmv.svg" alt="">
+            </div>
+        </div> -->
+
+        <section class="content-vid">
+            <div class="cont-vidFl">
+                <div class="text">
+                  <h2>Trabajamos con:</h2>
+                    <p>
+                        <span class="word wisteria">Innovación</span>
+                        <span class="word belize">Compromiso</span>
+                        <span class="word pomegranate">Profesionalidad</span>
+                        <span class="word green">Calidad</span>
+                        <span class="word midnight">Garantia</span>
+                    </p>
+                </div>
+                <div class="vid-flotante">
+                    <video loading="lazy" src="assets/vid/bg_principal.mp4" muted autoplay loop type="mp4"></video>
+                </div>
+                <div class="elementos">
+                    <div class="elemento mouseAnim">
+                        <a href="#cards">
+                            <div class="mouse"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- <video loading="lazy" src="assets/vid/bg_principal.mp4" autoplay muted loop>
+            </video> -->
+        </section>
+        <div class="wave-bottom" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150"
+                preserveAspectRatio="none" style="height: 100%; width: 100%;">
+                <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+                    style="stroke: none; fill: #fff;"></path>
+            </svg></div>
+    </header>
+    <main>
+        <section class="acerca-de contenedor">
+            <h3>Tenemos asombrosos servicios</h3>
+            <p class="intro">Trabajamos con calidad y calidez para ofrecerte productos y servicios en la <strong>rama
+            metalmecánica</strong> altamente competitivos y de su entera satisfacción. <br> Todas nuestras áreas trabajan en sincronía
+            desde el diseño de nuevos productos metálicos pasando por las distintas áreas de fabricación con un terminado fino y
+            de calidad con la <strong>pintura en polvo termoendurecida.</strong></p>
+            <div class="contenedor-acerca-de">
+                <div class="card-acerca">
+                  <a href="#">
+                    <div class="img">
+                      <!-- Generator: Adobe Illustrator 23.0.5, SVG Export Plug-In  -->
+                      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      	 width="120px" height="120px" viewBox="0 0 1156.12 1175.71" style="enable-background:new 0 0 1156.12 1175.71;"
+                      	 xml:space="preserve">
+                      <style type="text/css">
+                      	.st0{fill:#E84A1D;}
+                      </style>
+                      <defs>
+                      </defs>
+                      <path class="st0" d="M412.32,136.35l89.81-33.48L531.52,0H634.4l19.6,95.53l71.03,40.82L827.9,68.18l80.83,42.05l-17.15,122.47
+                      	l48.99,53.89l111.76-39.79l52.45,94.17l-76.02,68.09l14.7,88.18l112.67,41.64v90.63l-105.32,22.04l-22.04,90.63l73.48,88.18
+                      	l-51.44,85.73l-90.63-20.41l-73.48,49.8l24.49,117.57l-71.03,44.09l-95.53-75.93l-90.63,26.94l-14.7,117.57H531.52l-34.29-110.22
+                      	l-84.91-29.39l-76.75,73.48l-93.08-58.79l29.39-97.98l-63.68-57.15l-85.73,25.31L56.34,842.6l68.58-80.01l-17.15-106.14L0,629.5
+                      	V529.07l105.32-31.84l17.15-88.18l-68.58-71.03l51.44-88.18l105.32,18.78l63.68-53.07l-29.39-93.08l78.38-56.34L412.32,136.35z
+                      	 M578.06,198.4c-215.09,0-389.46,174.37-389.46,389.46s174.37,389.46,389.46,389.46s389.46-174.37,389.46-389.46
+                      	S793.15,198.4,578.06,198.4z"/>
+                      </svg>
+                    </div>
+                    <h4>Diseño y Proyección</h4>
+                    <p>Sus ideas se trasladan al software requerido para optimizar todos los detalles sus procesos de
+                      <strong>fabricación de productos metálicos.</strong></p>
+                    <p>(Autocad, SolidWorks, Vectorizado, etc.)</p>
+                  </a>
+                </div>
+                <div class="card-acerca">
+                  <a href="#">
+                    <div class="img">
+                      <!-- Generator: Adobe Illustrator 23.0.5, SVG Export Plug-In  -->
+                      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      	 width="120px" height="120px" viewBox="0 0 845.45 841.59" style="enable-background:new 0 0 845.45 841.59;"
+                      	 xml:space="preserve">
+                      <style type="text/css">
+                      	.st0{fill:#E84A1F;}
+                      	.st1{fill:#E84A1F;stroke:#000000;stroke-miterlimit:10;}
+                      </style>
+                      <defs>
+                      </defs>
+                      <polygon class="st0" points="838.62,0 579.67,0 579.67,128.29 509.59,205.5 383.67,205.5 383.67,237.57 498.9,237.57 498.9,300.52
+                      	446.04,353.38 406.24,353.38 359.03,306.17 359.03,226.88 266.23,134.08 266.23,0 10.69,0 10.69,34.45 231.63,34.45 231.63,144.92
+                      	322.5,235.79 322.5,306.46 401.79,385.75 401.79,517.9 451.38,517.9 451.38,382.49 528,305.87 528,236.38 620.95,143.43
+                      	620.95,32.07 838.62,32.07 "/>
+                      <path class="st1" d="M838.62,342.1h-97.4v-97.4h97.4V342.1z M820.31,262.81h-60.78v60.78h60.78V262.81z"/>
+                      <path class="st1" d="M838.62,830.3h-97.4v-97.4h97.4V830.3z M820.31,751.02h-60.78v60.78h60.78V751.02z"/>
+                      <path class="st1" d="M108.09,830.3h-97.4v-97.4h97.4V830.3z M89.78,751.02H29v60.78h60.78V751.02z"/>
+                      <path class="st1" d="M108.09,342.1h-97.4v-97.4h97.4V342.1z M89.78,262.81H29v60.78h60.78V262.81z"/>
+                      <polygon class="st0" points="552.35,323.59 529.19,347.15 789.92,535.12 424.65,797.94 280.63,695.48 494.44,544.03 609.36,545.81
+                      	610.26,517.9 489.1,517.9 234.3,693.7 424.65,841.59 845.45,534.23 "/>
+                      <polygon class="st0" points="296.66,323.59 326.06,351.6 59.39,536.02 192.73,636.19 305.57,552.05 225.39,552.05 225.39,507.51
+                      	380.41,507.51 380.41,537.8 194.21,682.12 0,537.8 "/>
+                      <polygon class="st0" points="563.63,409.81 586.2,444.25 500.68,499.49 471.57,499.49 471.57,469.2 "/>
+                      <polygon class="st0" points="283.5,409.81 260.93,444.25 346.45,499.49 375.56,499.49 375.56,469.2 "/>
+                      </svg>
+                    </div>
+                    <h4>Manufactura</h4>
+                    <p>Materializamos sus ideas en el desarrollo de <strong>metálicos</strong>, empleando las maquinas y herramientas
+                      de vanguardia.</p>
+                    <p>(<strong>Corte Laser, Doblez, Soldadura</strong>, etc.)</p>
+                  </a>
+                </div>
+                <div class="card-acerca">
+                  <a href="#">
+                    <div class="img">
+                      <img src="assets/img/pistola.png" alt="" width="120px" height="120px">
+                    </div>
+                    <h4>Pintura Electrostática</h4>
+                    <p>La <strong>aplicación de Pintura Electrostática</strong> es el mejor de los acabados para presentar sus proyectos
+                      en una amplia gama de colores y texturas de <strong>pintura en polvo</strong>.</p>
+                  </a>
+                </div>
+            </div>
+        </section>
+        <section class="servicio-cont">
+            <div class="servicio">
+                <div class="img-servicio">
+                    <img src="assets/img/undraw_wall_post_83ul.svg" alt="" id="ilustracion1">
+                </div>
+                <article class="contenido-servicio">
+                    <h3>Hacemos la mejor para ti</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam dignissimos est laudantium
+                        praesentium aliquam voluptatum ab aperiam? Facilis fuga inventore quisquam? Magnam inventore
+                        officia doloribus, perspiciatis facilis rem ad ea.</p>
+                    <ul>
+                        <li>Servicio1</li>
+                        <li>Servicio2</li>
+                        <li>Servicio3</li>
+                        <li>Servicio4</li>
+                    </ul>
+                </article>
+            </div>
+        </section>
+        <section class="galeria contenedor">
+            <h3>Conoce nuestro trabajo</h3>
+            <p>Un vistazo a nuestro trabajo </p>
+            <article class="galerias">
+                <div class="galeria-img">
+                    <img src="assets/img/IMG_1.jpg" alt="">
+                    <div class="extra-galeria">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perferendis architecto
+                            similique voluptate dolor nihil saepe incidunt possimus repellat atque!</p>
+                    </div>
+                </div>
+                <div class="galeria-img">
+                    <img src="assets/img/IMG_2.jpg" alt="">
+                    <div class="extra-galeria">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perferendis architecto
+                            similique voluptate dolor nihil saepe incidunt possimus repellat atque!</p>
+                    </div>
+                </div>
+                <div class="galeria-img">
+                    <img src="assets/img/IMG_3.jpg" alt="">
+                    <div class="extra-galeria">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perferendis architecto
+                            similique voluptate dolor nihil saepe incidunt possimus repellat atque!</p>
+                    </div>
+                </div>
+                <div class="galeria-img">
+                    <img src="assets/img/IMG_4.jpg" alt="">
+                    <div class="extra-galeria">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perferendis architecto
+                            similique voluptate dolor nihil saepe incidunt possimus repellat atque!</p>
+                    </div>
+                </div>
+                <div class="galeria-img">
+                    <img src="assets/img/IMG_5.jpg" alt="">
+                    <div class="extra-galeria">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perferendis architecto
+                            similique voluptate dolor nihil saepe incidunt possimus repellat atque!</p>
+                    </div>
+                </div>
+                <div class="galeria-img">
+                    <img src="assets/img/IMG_6.jpg" alt="">
+                    <div class="extra-galeria">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perferendis architecto
+                            similique voluptate dolor nihil saepe incidunt possimus repellat atque!</p>
+                    </div>
+                </div>
+            </article>
+        </section>
+        <section class="precios">
+            <h3>Tabla de precios</h3>
+            <div class="precios-tablas contenedor">
+                <div class="tabla">
+                    <h5>Basico</h5>
+                    <p class="price"><span>$10</span>/por mes</p>
+                    <ul>
+                        <li>Opciones</li>
+                        <li>Opciones</li>
+                        <li>Opciones</li>
+                    </ul>
+                    <a href="#">Comprar</a>
+                </div>
+                <div class="tabla">
+                    <h5>Basico</h5>
+                    <p class="price"><span>$10</span>/por mes</p>
+                    <ul>
+                        <li>Opciones</li>
+                        <li>Opciones</li>
+                        <li>Opciones</li>
+                    </ul>
+                    <a href="#">Comprar</a>
+                </div>
+                <div class="tabla">
+                    <h5>Basico</h5>
+                    <p class="price"><span>$10</span>/por mes</p>
+                    <ul>
+                        <li>Opciones</li>
+                        <li>Opciones</li>
+                        <li>Opciones</li>
+                    </ul>
+                    <a href="#">Comprar</a>
+                </div>
+            </div>
+        </section>
+        <div class="slider-contenedor">
+            <section class="slider">
+                <div class="cont-slide">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ea quas tempora quis nihil
+                        error sit, repellat minima voluptatem vel?</p>
+                        <img src="assets/img/1.jpg" alt="">
+                        <h4>Nombre 1</h4>
+                </div>
+                <div class="cont-slide">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ea quas tempora quis nihil
+                        error sit, repellat minima voluptatem vel?</p>
+                        <img src="assets/img/3.jpg" alt="">
+                        <h4>Nombre 2</h4>
+                </div>
+                <div class="cont-slide">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ea quas tempora quis nihil
+                        error sit, repellat minima voluptatem vel?</p>
+                        <img src="assets/img/4.jpg" alt="">
+                        <h4>Nombre 3</h4>
+                </div>
+            </section>
+        </div>
+    </main>
+    <footer>
+        <div class="footer-contact">
+            <h6>Contacame</h6>
+            <p>correo@correo.com</p>
+            <p>+555555555</p>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-youtube"></i>
+            <i class="fab fa-twitter"></i>
+        </div>
+        <div class="footer-logo">
+            <img src="assets/logo.svg" alt="">
+        </div>
+    </footer>
+    <script src="js/app.js"></script>
+    <script src="https://kit.fontawesome.com/950ee79485.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
